dcb67b0c836bac1d6f8b60c7e43fecd8a7b5d882
miguel7799 <42215165+miguel7799@users.noreply.github.com>
2020-04-17 21:16:12 -0500

    Programas completos

---
+++ Sources/CercoConvexo.cpp
@@ -0,0 +1,113 @@
+/*
+    * -Miguel Ángel Hernández Maldonado-
+    * 
+    * Este archivo es parte de https://github.com/tec-csf/tc2017-t3-primavera-2020-miguel7799
+    *
+    * Desarrollado para la calse de Analisis y Diseño de Algoritmos.
+    * Consulte el archivo README ubicado en el repositorio para
+    * obeter más detalles sobre la propiedad y datos especificos del código
+    * 
+    * Problema 4: Se desea implementar un algoritmo que permita resoler el problema de
+    * encontrar el cerco convexo de un conjunto de puntos en el plano XY
+    *
+    * Cerco convexo: Secuencia de puntos del conjunto que define una figura convexa
+    * que los encierra
+*/
+
+#include <iostream>
+#include <iomanip>
+
+//  * define N "": caidad de puntos que tiene la matriz por lado
+#define N 3
+using namespace std;
+
+/* 
+    * Implementacion de la case Cerco Convexo para implrirmir 
+    * la mariz dada por el usuario donde vien la figura que desa 
+    * caulcular y para encontrar y mostrar los puntos que forman
+    * el cerco convexo de dicha figura.
+    *
+    * Metodos Principales(printMatriz, busquedaPuntos).
+    * 
+    * El método printCercoConvexo une todos los métodos 
+    * en uno solo.
+    * 
+    * la función main() para este programa es ordenada y simple.
+   
+*/
+class CercoConvexo{
+    public:
+
+/*  
+    * printMatriz: imprime la matriz done se encuentra la figura
+    * de la cual se desea calcular el "Cerco Convexo".
+    * @param int Matriz[][]: matriz con la figura dibujada con "1" y "0".
+    * @param int f: número de fila.
+    * @param int c: número de columna.
+    * @return void
+*/
+        void printMatriz(int Matriz[N][N]){
+            for(int f=0; f<N; f++){
+                for(int c=0; c<N; c++){
+                    cout<<setw(4)<<Matriz[f][c] <<setw(4);
+                }cout<<endl;
+            }
+        }//end printMatriz
+
+/*
+    * busquedaPuntos: Es el método calve ya que Busca los puntos marcados como "1" 
+    * dentro de la matriz.
+    * @param int Matriz[][]: matriz con la figura dibujada con "1" y "0".
+    * @param int num: es el número que se usa para sustituir a los puntos 
+    * dentro de la matriz.
+    * @param int contador: cuenta el número de puntos de la figura.
+    * @param bool verifica: booleano para verificar que el numero 
+    * de int num, conside con el de la matriz.
+    * @param int co: columna donde esta el punto.
+    * @param int fi: fila donde esta el punto.
+    * @return punto
+*/
+        int busquedaPuntos(int Matriz[N][N], int num){
+        cout<<"El Cerco Convexo es: ";    
+        bool verifica;
+        int contador = 0;
+            for(int f=0; f<N; f++){
+                for(int c=0; c<N; c++){
+                    if(num == Matriz[f][c]){
+                    int co= c;
+                    int fi = f;
+                        verifica = true;
+                        if(verifica == true){
+                            cout<<fi+1<<","<< co+1<<" ,";
+                            contador ++;
+                        }else{
+                            cout<<"Valor no encontrado"<<endl;
+                        }     
+                    }
+                }
+            }
+                cout<<endl;
+                cout<<"Es una figura de: "<<contador <<" lados"<<endl;
+        }//end busquedaPuntos
+
+/*
+    * printCercoConvexo: Junta los métodos y es donde el usuario inserta la
+    * figura de la cual quiere conocer el Cerco Convexo.
+    * @param int matriz: matriz donde se dubujará mediante 1 y 0 la figura.
+    * 
+
+*/
+        int printCercoConvexo(){
+        int Matriz [N][N] = { { 0, 1, 0 }, 
+                              { 0, 0, 0 }, 
+                              { 1, 0, 1 } };
+        printMatriz(Matriz);
+        busquedaPuntos(Matriz,1);
+        }//end printCercoConvexo
+};
+int main(){
+CercoConvexo c;
+cout<<"Figura:"<<endl;
+c.printCercoConvexo();
+return 0;
+}
---
+++ Sources/RecorridoRey.cpp
@@ -0,0 +1,157 @@
+/*
+    * -Miguel Ángel Hernández Maldonado-
+    * 
+    * Este archivo es parte de https://github.com/tec-csf/tc2017-t3-primavera-2020-miguel7799
+    *
+    * Desarrollado para la calse de Analisis y Diseño de Algoritmos.
+    * Consulte el archivo README ubicado en el repositorio para obeter más detalles
+    * sobre la propiedad y datos especificos del código
+    * 
+    * Problema 7: Dado un tablero de ajedrez de tamano n*n, un rey es colocado en una casila
+    * arbitraria de coordenadas (x,y). Determinar los n2-1 movimientos del rey, de forma que
+    * pase por todas las casillas del tablero solo una vez.
+    * 
+*/
+#include <iostream>
+#include <stdio.h>
+#include <stdbool.h>
+#include <iomanip>
+using namespace std;
+
+/*  
+    * define X "": Ancho del tablero.
+    * define Y ""; Largo del tablero.
+*/
+#define X 4
+#define Y 4
+
+/* 
+    * Implementacion de la case RecorridoRey para identificar cuales
+    * serán los movimientos que tiene que realizar el rey para recorrer
+    * todo el tablero y también identifica si la solución es posible o no.
+    *
+    * Metodos Principales(movimiento rey).
+    * 
+    * Hay métodos auxiliares (limite y solución tablero) que ayudan a
+    * platear los limites de movimiento que puede hacer el rey y como
+    * se distribuyen dichos movimientos
+    * 
+    * El método RecorridoRey une todos los métodos 
+    * en uno solo.
+    * 
+    * la función main() para este programa es ordenada y simple.
+   
+*/
+class RecorridoRey{
+    public: 
+
+/*  
+    * movimientoRey: calcula cual será el siguiente movimiento del rey.
+    * @param int tablero[][]: matriz del tablero
+    * @param int x: casilla columna.
+    * @param int y: casilla fila
+    * @param int movimiento: numeor de movimiento
+    * @param int movx[]: casilla del movimiento en la columna
+    * @param int movy[]: casilla del movimiento en la fila
+    * @param int nextx: siguiente movimiento en x
+    * @param int mexty: siguiente movimiento en y
+    * @return movimiento
+*/
+            int movimientoRey (int x, int y, int movimiento, int tablero[X][Y], int movx[X], int movy[Y] ){
+                int m, nextx, nexty, contador; 
+                if (movimiento == X*Y) 
+                    return 1;
+                for (m = 0; m < 4; m++) 
+                { 
+                    nextx = x + movx[m]; 
+                    nexty = y + movy[m]; 
+
+                    if (limite(nextx, nexty, tablero)) 
+                    { 
+                        tablero[nextx][nexty] = movimiento; 
+                        
+                        if (movimientoRey(nextx, nexty, movimiento+1, tablero, movx, movy) == 1) 
+                            return 1; 
+                        else
+                            tablero[nextx][nexty] = -1;
+                            
+                    } 
+                    
+                }
+                
+                return 0; 
+                
+        }//end movimientoRey
+/*  
+    * limite: calcula los limites del tablero
+    * @param int tablero[][]: matriz del tablero
+    * @param int x: casilla columna.
+    * @param int y: casilla fila
+    * @return limite
+*/
+        int limite(int x, int y, int tablero[X][Y]){
+            return ( x >= 0 && x < X && y >= 0 && y < Y && tablero[x][y] == -1);
+        } //end limite
+
+/*  
+    * solucionTablero: imprime los movimientos en el teblero
+    * @param int Matriz[][]: matriz del tablero
+    * @return void
+*/
+        void solucionTablero(int tablero [X][Y]){ 
+            for (int x = 0; x < X; x++)
+            { 
+                for (int y = 0; y < Y; y++) 
+                    cout<<setw(4)<<tablero[x][y]; 
+                    cout<<endl;
+                         
+            }   
+            
+        }// end solucionTablero
+/*
+    * int recorridoRey: Junta los métodos y nos da la solución
+    * @param int inicio [][]: matriz del tablero
+    * @param int inicio [X][Y]; posicione ne la que inicia el recorrido
+    * @param int movx[]: cordenadas posibles para el movimiento en x
+    * @param int movy[]: cordenadas posibles para el movimiento en y
+    * @param
+
+*/
+    
+        int recorridoRey(){
+            int inicio [X][Y];
+            
+            for (int x = 0; x < X; x++) 
+                for (int y = 0; y < Y; y++) 
+                    inicio[x][y] = -1;  
+                    
+                             
+            int movx[4] = { 1, -1, 0, 0 }; 
+            int movy[4] = { 0, 0, 1, -1 }; 
+ 
+            inicio[0][0] = 0; 
+            
+            cout<< "El rey iniciará en la posición: 0,0"<< endl;
+            cout<< "El tablero tiene un tamaño de: "<<X<<setw(1)<<"*"<<Y <<endl;
+            cout<< "El rey hará" <<" "<< (X*Y)<<" "<< "movimientos"<<endl;
+            if (movimientoRey(0, 0, 1, inicio, movx, movy) == 0) 
+            { 
+                cout<<"No tiene solución"<<endl;  
+            } 
+            else
+                solucionTablero(inicio); 
+                
+                
+        
+            return 1; 
+        }
+};
+int main(){
+    RecorridoRey r;
+    r.recorridoRey();
+    return 0;
+}
+
+
+
+
